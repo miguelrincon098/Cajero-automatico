73738514c4dd06aa7d79a5292abc7ebea377e6bb
miguelrincon098 <miguelmrincon1@gmail.com>
2023-08-31 21:58:50 -0600

    second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const user = document.getElementById("usuario");
+const password = document.getElementById("contrasena");
+const button = document.getElementById("ingresar");
+const button = document.getElementById("consultar");
+const button = document.getElementById("ingresar-monto");
+const button = document.getElementById("retirar");
+
+let indice = -1;
+let users = [
+  {
+    nombre: "usuario 1",
+    contrasena: 1234,
+    saldo: 200,
+  },
+  {
+    nombre: "usuario 2",
+    contrasena: 2345,
+    saldo: 290,
+  },
+  {
+    nombre: "usuario 3",
+    constrasena: 3456,
+    saldo: 67,
+  },
+];
+
+
+    
+    
+    
