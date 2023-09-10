bfd7db211576b71b223a330bff1a5d0fe885d225
miguelrincon098 <miguelmrincon1@gmail.com>
2023-09-10 02:13:36 -0600

    Segundo commit del cajero

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const user = document.getElementById("usuario");
-const password = document.getElementById("contrasena");
-const button = document.getElementById("ingresar");
-const button = document.getElementById("consultar");
-const button = document.getElementById("ingresar-monto");
-const button = document.getElementById("retirar");
+const username = document.getElementById('usuario');
+const password = document.getElementById('contrasena');
+const button = document.getElementById('ingresar');
 
-let indice = -1;
+button.addEventListener ('click' , (e) => {
+  e.preventDefault()
+  const data = {
+    username: username.value,
+    password: password.value, 
+  }
+})
+
 let users = [
   {
-    nombre: "usuario 1",
+    nombre: 'usuario 1',
     contrasena: 1234,
     saldo: 200,
   },
   {
-    nombre: "usuario 2",
+    nombre: 'usuario 2',
     contrasena: 2345,
     saldo: 290,
   },
   {
-    nombre: "usuario 3",
+    nombre: 'usuario 3',
     constrasena: 3456,
     saldo: 67,
   },
-];
-
-
-    
-    
-    
+]; 
